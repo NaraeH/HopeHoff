ae207bbcfeb46548b25cd8dad7ac7d7b55807fcb
Ban <Ban@192.168.0.125>
2014-12-19 17:03:19 -0800

    2014.12.19 [반기백]로그인페이지 완료

--- hopeHoff/src/main/webapp/js/login/login.js
+++ hopeHoff/src/main/webapp/js/login/login.js
@@ -36,6 +36,22 @@ $(document).ready(function(){
 		console.log("display:true");
 	});
 	
+	
+	// Node.js랑 연결되어있다. 아이디 중복체크임
+	// 키를 눌렀다가 내 손에 모든게 떨어졌을때
+	$("#formGroupInputLarge").keyup(function(){
+		
+		$.get("http://localhost:3000/loginCheck",{
+			// 겟 요청의 파라미터를 이것을 전달하겠다. 이위에꺼  //겟터
+			id : $('#formGroupInputLarge').val()
+		}, function(data){
+			//노드에 서버에 응답까지 성공하면  //세터
+			//이렇게 응답하겠다.(이렇게 이벤트발생하겠다.)
+			$('#yesno').html(data.result);
+		});
+		
+	});
+	
 	/*
 	$(":input").click(function(){
 		console.log($(this));
