5bda9a1e9664865d036439fd8645d91e4fdb37ab
지용근 <wldydrms11@gmail.com>
2014-12-22 21:31:40 +0900

    2014 12 22 v1.0.0 지용근 myWrite 함수 구현 완료..

--- hopeHoff/src/main/webapp/js/myWrite/myWrite.js
+++ hopeHoff/src/main/webapp/js/myWrite/myWrite.js
@@ -1,13 +1,19 @@
-	var count= 3;
+$(document).ready(function(){
+	
+	
+var count= 3;
+	
+	 /***************************** table에 행만들기. ********************************/
 		for(var i=0; i<=count*2; i++){
 			$('<tr id=tRow'+i+'>').appendTo($('#dataForm'));
 			if(i==0){
 				for(var j=0;j<5; j++){
 					$('<th class=tableData'+i+'>').html("<input type=\"checkbox\">").appendTo($('#tRow0'));
-					$('<th class=tableData'+i+'>').html("No").appendTo($('#tRow0'));
-					$('<th class=tableData'+i+'>').html("Phone-Number").appendTo($('#tRow0'));
-					$('<th class=tableData'+i+'>').html("People-Count").appendTo($('#tRow0'));
-					$('<th class=tableData'+i+'>').html("Time").appendTo($('#tRow0'));
+					$('<th class=tableData'+i+'>').html("번호").appendTo($('#tRow0'));
+					$('<th class=tableData'+i+'>').html("전화 번호").appendTo($('#tRow0'));
+					$('<th class=tableData'+i+'>').html("인원").appendTo($('#tRow0'));
+					$('<th class=tableData'+i+'>').html("시간").appendTo($('#tRow0'));
+					$('<th class=tableData'+i+'>').html("비고").appendTo($('#tRow0'));
 					break;
 				}
 			}
@@ -18,54 +24,41 @@
 					$('<td class=tableData'+i+'>').html("Phone-Number").appendTo($('#tRow'+i));
 					$('<td class=tableData'+i+'>').html("People-Count").appendTo($('#tRow'+i));
 					$('<td class=tableData'+i+'>').html("Time").appendTo($('#tRow'+i));
+					$('<td class=tableButton'+i+'>').html("<button id=dataButton"+i+" class=\"dataButton\"></button>").appendTo($('#tRow'+i));
+					$('.dataButton').css('background-image','url(../../img/common/arrow_down.png)')
+									.css('width','25px')
+									.css('height','20px')
+									.css('background-position','-1260px -5120px');
 					break;
 				}
 			}
 			else{
-				 $('<td colspan=\"5\" class=tableShow id=tableContent'+i+' >').html(" <p align=center> 내용 </p> ").appendTo($('#tRow'+i));
+				 $('<td colspan=\"6\" class=tableShow id=tableContent'+i+' >').html(" <p align=center> 내용 </p> ").appendTo($('#tRow'+i));
 			}
 		}
 		  for(var i=2; i<=count*2; i+=2){
 			$('#tableContent'+i).css('display','none');
 		} 
+			 /***************************** 홀수열 눌렀을때 짝수열 내용보기 ********************************/
+
+			  $('.dataButton').click(function(event){
+				  var num =$($(this)[0]).attr("id").split("dataButton")[1]-0;
+			
+				var status=$('#'+'tableContent'+(num+1)).css('display');
+				var tableContents = $('#'+'tableContent'+(num+1));
+				console.log(tableContents);
+				 if(status=='none')
+					 {
+					 	$('#'+'tableContent'+(num+1)).css('display','');
+					 }
+				 else
+					 $('#'+'tableContent'+(num+1)).css('display','none');
+			 });
+			 
 		
-		 for(var i=1; i<=count*2; i+=2){
-			 
-			 if(i==1){
-			 $('#tRow1').click(function(event){
-					 var status=$('#tableContent2').css('display');
-				 if(status=='none')
-					 {
-					 	$('#tableContent2').css('display','');
-					 }
-				 else
-					$('#tableContent2').css('display','none');
-			 });
-			}
-			 
-			 if(i==3){
-			 $('#tRow3').click(function(event){
-				 var status=$('#tableContent4').css('display');
-				 if(status=='none')
-					 {
-					 	$('#tableContent4').css('display','');
-					 }
-				 else
-					$('#tableContent4').css('display','none');
-			 });
-			}
-			 
-			 if(i==5){
-				 $('#tRow5').click(function(event){
-					 var status=$('#tableContent6').css('display');
-					 if(status=='none')
-						 {
-						 	$('#tableContent6').css('display','');
-						 }
-					 else
-						$('#tableContent6').css('display','none');
-				 });
-				}
-		 }
+			
+			});
 		 
+		 //***************************** 삭제 버튼 눌렀을시에********************************//
 		 
+		
