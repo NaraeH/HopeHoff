ba890f2efbba8faaafb7ad0c472497fd356691d6
지용근 <wldydrms11@gmail.com>
2014-12-31 09:27:31 +0900

    2014 12 31 v.1.0.0 지용근 domain 생성

--- hopeHoff/src/main/java/hopeHoff/domain/Reservation.java
+++ hopeHoff/src/main/java/hopeHoff/domain/Reservation.java
@@ -9,4 +9,45 @@ public class Reservation {
 	protected Date reservationDate;
 	protected String reservationContent;
 	protected String userId;
+	
+	@Override
+	public String toString() {
+		return "Reservation [reservationNo=" + reservationNo + ", businessNo="
+				+ businessNo + ", reservationDate=" + reservationDate
+				+ ", reservationContent=" + reservationContent + ", userId="
+				+ userId + "]";
+	}
+	
+	public int getReservationNo() {
+		return reservationNo;
+	}
+	public void setReservationNo(int reservationNo) {
+		this.reservationNo = reservationNo;
+	}
+	public String getBusinessNo() {
+		return businessNo;
+	}
+	public void setBusinessNo(String businessNo) {
+		this.businessNo = businessNo;
+	}
+	public Date getReservationDate() {
+		return reservationDate;
+	}
+	public void setReservationDate(Date reservationDate) {
+		this.reservationDate = reservationDate;
+	}
+	public String getReservationContent() {
+		return reservationContent;
+	}
+	public void setReservationContent(String reservationContent) {
+		this.reservationContent = reservationContent;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	
 }
--- hopeHoff/src/main/java/hopeHoff/domain/Shop.java
+++ hopeHoff/src/main/java/hopeHoff/domain/Shop.java
@@ -14,4 +14,85 @@ public class Shop {
 	protected String shopType;
 	protected String shopSnack;
 	
+	
+	
+	@Override
+	public String toString() {
+		return "Shop [businessNo=" + businessNo + ", userId=" + userId
+				+ ", shopName=" + shopName + ", shopAddress=" + shopAddress
+				+ ", shopPhone=" + shopPhone + ", shopTime=" + shopTime
+				+ ", shopIntro=" + shopIntro + ", shopPhoto=" + shopPhoto
+				+ ", shopArea=" + shopArea + ", shopType=" + shopType
+				+ ", shopSnack=" + shopSnack + "]";
+	}
+	
+	public String getBusinessNo() {
+		return businessNo;
+	}
+	public void setBusinessNo(String businessNo) {
+		this.businessNo = businessNo;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getShopName() {
+		return shopName;
+	}
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+	public String getShopAddress() {
+		return shopAddress;
+	}
+	public void setShopAddress(String shopAddress) {
+		this.shopAddress = shopAddress;
+	}
+	public String getShopPhone() {
+		return shopPhone;
+	}
+	public void setShopPhone(String shopPhone) {
+		this.shopPhone = shopPhone;
+	}
+	public String getShopTime() {
+		return shopTime;
+	}
+	public void setShopTime(String shopTime) {
+		this.shopTime = shopTime;
+	}
+	public String getShopIntro() {
+		return shopIntro;
+	}
+	public void setShopIntro(String shopIntro) {
+		this.shopIntro = shopIntro;
+	}
+	public String getShopPhoto() {
+		return shopPhoto;
+	}
+	public void setShopPhoto(String shopPhoto) {
+		this.shopPhoto = shopPhoto;
+	}
+	public String getShopArea() {
+		return shopArea;
+	}
+	public void setShopArea(String shopArea) {
+		this.shopArea = shopArea;
+	}
+	public String getShopType() {
+		return shopType;
+	}
+	public void setShopType(String shopType) {
+		this.shopType = shopType;
+	}
+	public String getShopSnack() {
+		return shopSnack;
+	}
+	public void setShopSnack(String shopSnack) {
+		this.shopSnack = shopSnack;
+	}
+	
+	
+	
 }
