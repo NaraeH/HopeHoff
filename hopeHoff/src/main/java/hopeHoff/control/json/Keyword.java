e031dc102b00f8861d24d7a22c2253f041e60d64
Narae <Narae@Narae-PC>
2014-12-31 16:07:46 +0900

    20141231 v1.0.0 -narae 1) main keyword json 바꾸는 작업중

--- hopeHoff/src/main/java/hopeHoff/control/json/Keyword.java
+++ hopeHoff/src/main/java/hopeHoff/control/json/Keyword.java
@@ -1,21 +1,30 @@
 package hopeHoff.control.json;
 
 import java.io.FileReader;
+import java.util.HashMap;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("json/keyword")
 public class Keyword {
 	
 
+	@RequestMapping("/keyword")
 	public Object keywords(){
 		JSONArray region= null;
+		
 		try{
-			JSONParser parser = new JSONParser();            C:\Users\Narae\git\HopeHoff.git\hopeHoff\src\main\java\hopeHoff\control\json\Keyword.java
-			JSONObject keyword = (JSONObject) parser.parse(new FileReader("C:/Users/Narae/git/HopeHoff.git/hopeHoff/src/main/java/keyword.json"));
+			JSONParser parser = new JSONParser();            
+			String path = Keyword.class.getResource("").getPath();
+			JSONObject keyword = (JSONObject) parser.parse(new FileReader(path  + "keyword.json"));
 			
-			region = (JSONArray) keyword.get("지역");
+			//region = (JSONArray) keyword.get("region");
+			
+			HashMap<String, Object> keywords = new HashMap<String, Object>();
+			keywords.put("region", keyword.get("region"));
 			
 		}catch(Exception e){
 			e.printStackTrace();
@@ -24,12 +33,9 @@ public class Keyword {
 	}
 	
 	public static void main(String[] args) {
+
 		Keyword keyword = new Keyword();
 		
 		System.out.println(keyword.keywords());
 	}
-			
-	
-	
-
 }
