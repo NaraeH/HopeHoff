019195b36072e8c2818d7900d8620ba6676c4209
yeonjungbae <yeonjungbae@yeonjungbae>
2015-01-02 14:26:51 +0900

    연정 새롭게시작합니당~

--- hopeHoff/src/main/webapp/js/login/login.js
+++ hopeHoff/src/main/webapp/js/login/login.js
@@ -58,6 +58,7 @@ $(function(){
 			
 				if(data.result == "사용가능한 아이디입니다."){
 					$('#yesno').html(data.result).css("color","green");
+					$('#btnSignUp').attr("disabled", false);
 				} else { /*중복된 아이디가 있습니다.*/
 					$('#yesno').html(data.result).css("color","red");
 					$('#btnSignUp').attr("disabled", true);
@@ -74,8 +75,24 @@ $(function(){
 	$('#formGroupInputLarge').focusout(function() {
 		$('#yesno').css("display","none");
 	});
-	
-	
+	/* ----------------------------- */
+	$('#inputPassword3').focus(function() {
+		 $('#pwdValid').css("display","");
+		 $("#inputPassword3").keyup(function(){
+			 if(($('#inputPassword3').val().match(/([a-zA-Z0-9].*[!,@,#,$,%,^,&,*,?,_,~])|([!,@,#,$,%,^,&,*,?,_,~].*[a-zA-Z0-9])/) != null)
+					 && (($('#inputPassword3').val().length >= 6)&&($('#inputPassword3').val().length <= 16) )){
+				
+				 $('#pwdValid').html("ok").css("color","green");
+		   
+			 } else {
+				 $('#pwdValid').html("문자, 숫자, 특수문자(!,@,#,$,%,^,&,*,?,_,~의 조합으로 6~16자리").css("color","red");
+				 $('#btnSignUp').attr("disabled", false);
+			 }
+		 });
+	});
+	$('#inputPassword3').focusout(function() {
+		$('#pwdValid').css("display","none");
+	});
 	/*********************/
 	$('#checkPassword').focus(function() {
 		 $('#pwdCheckText').css("display","");
@@ -84,6 +101,8 @@ $(function(){
 		
 			 if(($('#inputPassword3').val() == $('#checkPassword').val()) && ($('#inputPassword3').val()!= "")){
 				 $('#pwdCheckText').html("일치합니다.").css("color","green");
+				 $('#btnSignUp').attr("disabled", false);
+
 		
 			 } else {
 				 $('#pwdCheckText').html("불일치ㅠ_ㅠ").css("color","red");
@@ -98,6 +117,36 @@ $(function(){
 	});
 	/********************/
 	
+	
+	
+	/* 연정아 여기부터당~~~^.^*/
+/*	// 전화번호 앞자리는 2~3자리 숫자, 두번째 자리는 3~4자리 숫자
+    // 세번째 자리는 4자리 숫자
+	$('#phoneNumber').focus(function() {
+		 $('#phoneNumValid').css("display","");
+		
+		 $("#checkPassword").keyup(function(){
+	
+           if (!$('#phoneNumber').val().match(/^0(2|1[01679])$/)) {
+        	   if (!$('#phoneNumber').val().match(/^[0-9]{3,}$/)) {
+        		   if (!$('#phoneNumber').val().match(/^[0-9]{4}$/)){
+        			   $('#phoneNumValid').html("ok.").css("color","green");
+      				   $('#btnSignUp').attr("disabled", false);
+        		   }
+        	   }
+            } else {
+               $('#phoneNumValid').html("앞자리는 2~3자리 숫자, 두번째 자리는 3~4자리 숫자 세번째 자리는 4자리 숫자").css("color","red");
+				 $('#btnSignUp').attr("disabled", true);
+            	
+            }
+		 });
+	});
+	
+	$('#phoneNumber').focusout(function() {
+		$('#phoneNumValid').css("display","none");
+	});*/
+	
+	/* .............................................*/
 	$('#btnCancel').click(function(){
 		$("input[name='inlineRadioOptions']").filter("[value='option1']").prop("checked",false);
 		$("input[name='inlineRadioOptions']").filter("[value='option2']").prop("checked",false);
@@ -135,5 +184,60 @@ $(function(){
 		console.log("check");
 	});
 	*/
+	
+	
+	
+/*    // 이메일 검사
+    // 4글자 이상(\w = [a-zA-Z0-9_], [\w-\.]) @가 나오고
+    // 1글자 이상(주소). 글자 가 1~3번 반복됨
+    if(!chk(/^[\w]{4,}@[\w]+(\.[\w-]+){1,3}$/, email, "이메일 형식에 어긋납니다."))
+           return false;
+   
+    // 아이디 검사
+    // 첫 글자는 반드시 영문 소문자, 4~12자로 이루어지고, 숫자가
+    // 하나 이상 포함되어야 한다. 영문소문자와 숫자로만 이루어져야한다.
+    // \d : [0-9]와 같다.       {n,m} : n에서 m까지 글자수
+    if(!chk(/^[a-z][a-z\d]{3,11}$/, id, "첫글자는 영문 소문자, 4~12자 입력할것!"))
+           return false;
+    if(!chk(/[0-9]/, id, "숫자 하나이상포함!"))
+           return false;
+
+    // 비밀번호 확인 검사
+    if(pass.value!=pass1.value) {
+           alert("비밀번호 확인!");
+    }
+   
+    // 이름 검사
+    // 2글자 이상, 한글만
+    // 통과하지 못하면 한글로 2글자 이상을 넣으세요~ alert 출력!
+    if(!/^[가-힝]{2,}$/, name, "한글로 2글자 이상을 넣으세요~")
+           return false;
+   
+    // 전화번호 검사
+
+    // 전화번호 앞자리는 2~3자리 숫자, 두번째 자리는 3~4자리 숫자
+    // 세번째 자리는 4자리 숫자
+
+    if (tel1.value != '') {
+           if (!chk(/^0(2|1[01679])$/, tel1, "번호 2자리 이상 입력"))
+                   return false;
+           if (!chk(/^[0-9]{3,}$/, tel2, "번호 3자리 이상 입력"))
+                   return false;
+           if (!chk(/^[0-9]{4}$/, tel3, "4자리 번호 입력"))
+                   return false;
+           }
+
+    }
+
+    function chk(re, e, msg) {
+           if (re.test(e.value)) {
+                   return true;
+           }
+
+           alert(msg);
+           e.value = "";
+           e.focus();
+           return false;
+    }*/
 });
 
