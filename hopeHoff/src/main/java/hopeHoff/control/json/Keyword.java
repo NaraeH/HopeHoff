0aa9a7b823b13dc8fc44bb005e8b7643855acce4
Narae <Narae@Narae-PC>
2015-01-05 13:40:40 +0900

    20150105 v1.0.0 -narae 중간 커밋

--- hopeHoff/src/main/java/hopeHoff/control/json/Keyword.java
+++ hopeHoff/src/main/java/hopeHoff/control/json/Keyword.java
@@ -17,8 +17,9 @@ public class Keyword {
 		
 		try{
 			JSONParser parser = new JSONParser();            
-			String path = Keyword.class.getResource("").getPath();
-			JSONObject keyword = (JSONObject) parser.parse(new FileReader(path  + "keyword.json"));
+			String path = Keyword.class.getResource("../../domain/keyword.json").getPath();
+			
+			JSONObject keyword = (JSONObject) parser.parse(new FileReader(path));
 			
 			keywords = new HashMap<String, Object>();
 			keywords.put("region", keyword.get("region"));
