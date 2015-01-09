2ea86b4dabb714278d69a8d011306c864566ac27
yeonjungbae <yeonjungbae@yeonjungbae>
2015-01-09 16:56:46 +0900

    kk

--- loginCheck/app.js
+++ loginCheck/app.js
@@ -17,8 +17,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host    :'localhost',
     port : 3306,
-    user : 'study',
-    password : 'study',
+    user : 'root',
+    password : '1111',
     database:'hopeHoff'
 });
 
