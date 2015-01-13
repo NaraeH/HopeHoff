155ab8c9841d303d607bd6a7a088ff18cd124540
yeonjungbae <yeonjungbae@yeonjungbae>
2015-01-13 21:48:22 +0900

    연정 150113 v1.0.0 myPage에 id랑 name 받아와서 placeholder에 넣는거함 ㅅㄱ

--- hopeHoff/src/main/webapp/js/myPage/myPage.js
+++ hopeHoff/src/main/webapp/js/myPage/myPage.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(function(){
 	//******************** btnClose 클릭시에 ************************//
 	$("#btnMyPageClose").click(function(){
 		$("#back").css("display", "none");
@@ -6,3 +6,15 @@ $(document).ready(function(){
 	});
 
 });
+
+$.getJSON('/hopeHoff/json/auth/loginUser.do', function(data){
+    console.log("what the f***"+data.status);
+	if (data.status == "fail") {
+		console.log("fail..");
+	} else { //success
+		$(".userId").attr("placeholder"," "+data.loginUser.uId);
+		$(".userName").attr("placeholder"," "+data.loginUser.uName);
+	}
+});
+
+
