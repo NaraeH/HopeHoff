54a5995e66a88b9eeda98880e182cd637fd30f6a
Narae <Narae@Narae-PC>
2015-01-17 01:05:40 +0900

    20150117 v1.0.0 -narae
    1)회원정보 수정시에 좀 더 디테일하게 알림창 띄움

--- hopeHoff/src/main/webapp/js/myPage/myPage-change-table.js
+++ hopeHoff/src/main/webapp/js/myPage/myPage-change-table.js
@@ -1,51 +1,56 @@
-var pwdStatus = false;  //수정 버튼 눌렀을 때의 체크를 위함
-var randomNo = 0;
-var userId = $( ".userId" ).val();
-var updateUserMap = {password: "none", phoneNo: "none"};
+var randomNo = 0;                                          //핸드폰 인증번호
+var userId = $( ".userId" ).val();                         //user 아이디
+var updateUserMap = {password: "none", phoneNo: "none"};   //회원정보 수정될 값 저장
 
 
 $( "#sendBtn" ).click(function(){
 	randomNo = Narae.sendSms( $("#phoneNo").val() );
+	$( phoneNo ).attr("data-check", "validDataClick");
 	console.log("인증번호==>" + randomNo);
 });
 
 
-$('#confirmBtn').click(function(){
-	if($('#confirmVerCode').val() == randomNo){
-		 $('#confirmVerCode').css("border","green 2px solid");
-		 $( ".confimNoValid" ).css("display", "none");
-		 
-		 updateUserMap.phoneNo = $("#phoneNo").val();  //핸드폰 인증까지 만료 하면 변경될 핸드폰 값 넣기
-	 } else {
-		 $('#confirmVerCode').css("border","red 2px solid");
-		 $( ".confimNoValid" ).html("인증번호가 맞지 않습니다").css("display", "block").css("color", "red");
-	 }
-});
-
 $( "#btnPwdChange" ).click(function() {
-	if( updateUserMap.password == 'none' ){
-		alert("바꾸실 정보가 없습니다.");
-	}else {
+	var dataCheckNewPwd = $( "#newPwd" ).attr("data-check");
+	var dataCheckNewPwdConfirm = $( "#newPwdConfirm" ).attr("data-check");
+	
+	if( ( dataCheckNewPwd == 'validData' ) && ( dataCheckNewPwdConfirm == 'validData' ) && ( updateUserMap.password != 'none' )){
 	    $.post('../../json/auth/updateUser.do'
-		        , { userid: userId,
-				    password: updateUserMap.password }
+		        , { userId: userId,
+		        	password: updateUserMap.password }
 		        , function(data){
 		        	if(data.status) {
-		        		alert("비밀번호가 성공적으로 변경되었습니다.");
+		        		alert("암호가 성공적으로 변경되었습니다.");
 		        	}else {
-		        		alert("비밀번호가 변경에 실패하셨습니다.")
+		        		alert("암호가 변경에 실패하셨습니다.")
 		        	}
-		        }
-		        , 'json');
+		        }, 'json');
+	}else {
+		
+		if( ( dataCheckNewPwd == 'none' ) && ( dataCheckNewPwdConfirm == 'none' ) ){
+			alert("바꾸실 정보가 없습니다.");
+		}else if ( dataCheckNewPwd == 'invalidData' ){
+			alert("암호의 형식이 맞지 않습니다");
+			$( "#newPwd" ).focus();
+		}else if( ( dataCheckNewPwd == 'validData' ) && ( dataCheckNewPwdConfirm == 'none' ) ){
+			alert("암호를 다시 한번 입력해주세요");
+			$( "#newPwdConfirm" ).focus();
+		}else if( ( dataCheckNewPwd == 'validData' ) && ( dataCheckNewPwdConfirm == 'invalidData' ) ){
+			alert( "암호가 일치하지 않습니다 ");
+			$( "#newPwdConfirm" ).focus();
+		}else {
+			alert( "암호입력이 바르지 않습니다" );
+		}
 	}
 });
 
 $( "#btnPhoneNoChange" ).click(function() {
-	if( updateUserMap.phoneNo == 'none' ){
-		alert("바꾸실 정보가 없습니다.");
-	}else {
+	var dataCheckPhoneNo = $( "#phoneNo" ).attr("data-check");
+	var dataCheckConfirmVerCode = $( "#confirmVerCode" ).attr("data-check");
+	
+	if ( ( dataCheckPhoneNo == 'validDataClick' ) && ( dataCheckConfirmVerCode == 'validData' ) && ( updateUserMap.phoneNo != 'none' )){
 	    $.post('../../json/auth/updateUser.do'
-		        , { userid: userId,
+		        , { userId: userId,
 		        	phoneNo: updateUserMap.phoneNo }
 		        , function(data){
 		        	if(data.status) {
@@ -55,23 +60,41 @@ $( "#btnPhoneNoChange" ).click(function() {
 		        	}
 		        }
 		        , 'json');
+	}else {
+		if( ( dataCheckPhoneNo == 'none' ) && ( dataCheckConfirmVerCode == 'none' ) ){
+			alert("바꾸실 정보가 없습니다.");
+		}else if ( dataCheckPhoneNo == 'invalidData' ){
+			alert("핸드폰번호의 형식이 맞지 않습니다");
+			$( "#phoneNo" ).focus();
+		}else if ( dataCheckPhoneNo == 'validDataNoneClick' ){
+			alert("인증번호 전송버튼을 눌러주세요");
+		}else if( ( dataCheckPhoneNo == 'validDataClick' ) && ( dataCheckConfirmVerCode == 'none' ) ){
+			alert("인증번호를 입력해주세요");
+			$( "#confirmVerCode" ).focus();
+		}else if( ( dataCheckPhoneNo == 'validDataClick' ) && ( dataCheckConfirmVerCode == 'invalidData' ) ){
+			alert( "인증번호가 일치하지 않습니다 ");
+			$( "#confirmVerCode" ).focus();
+		}else {
+			alert( "암호입력이 바르지 않습니다" );
+		}
 	}
 });
 /* --------------password 유 효 성--------------- */
 $( '#newPwd' ).focus( function() {
 	var newPwd = $( "#newPwd" );
-		
 	$('.pwdValid').css("display","");
+	
 	$( newPwd ).keyup(function(){
 		if(($(newPwd ).val().match(/([a-zA-Z0-9].*[!,@,#,$,%,^,&,*,?,_,~])|([!,@,#,$,%,^,&,*,?,_,~].*[a-zA-Z0-9])/) != null)
 					 && (($( newPwd).val().length >= 6)&&($( newPwd ).val().length <= 16) )){
 			$( ".pwdValid" ).html("ok").css("color","green");
 			$( newPwd ).css("border","green 2px solid");
+			$( newPwd ).attr("data-check", "validData");
 	
 		} else {
 			$( ".pwdValid" ).html("문자, 숫자, 특수문자(!,@,#,$,%,^,&,*,?,_,~의 조합으로 6~16자리").css("color","red");
 			$( newPwd ).css("border","red 2px solid");
-				
+			$( newPwd ).attr("data-check", "invalidData");
 		}
 	});
 });
@@ -89,14 +112,15 @@ $( "#newPwdConfirm" ).focus( function() {
 	
 	$( newPwdConfirm ).keyup(function() {
 		if( newPwdConfirm.val() == $( "#newPwd" ).val() ){
-			pwdStatus = "success"; 
 			updateUserMap.password = newPwdConfirm.val(); //암호 일치까지 확인하면 변경될 암호 넣기
 
 			$( ".pwdConfirmValid" ).html("암호가 일치합니다").css("color", "green");
 			$( newPwdConfirm ).css("border","green 2px solid");
+			$( newPwdConfirm ).attr("data-check", "validData");
 		}else {
 			$( ".pwdConfirmValid" ).html("암호가 일치하지 않습니다").css("color", "red");
 			$( newPwdConfirm ).css("border","red 2px solid");
+			$( newPwdConfirm ).attr("data-check", "invalidData");
 		}
 	})
 });
@@ -114,33 +138,57 @@ $( "#newPwdConfirm" ).focusout( function() {
 $( "#phoneNo" ).focus(function() {
 	var phoneNo = $( "#phoneNo" );
 	
-	 //$('#phoneNumValid').css("display","");
-	 
 	 $( phoneNo ).keyup(function(){
 		 if( ( phoneNo ).val().match(/[01](0|1|6|7|8|9)[-](\d{4}|\d{3})[-]\d{4}$/) != null){
 			 $( ".phoneNoValid" ).html("ok").css("color","green");
 			 $( phoneNo ).css("border","green 2px solid");
+			 $( phoneNo ).attr("data-check", "validDataNoneClick");
 			 
 		 } else {
 			 $( ".phoneNoValid" ).html("유효한 핸드폰 번호가 아닙니다.  예)010-1234-4567").css("color","red");
 			 $( phoneNo ).css("border","red 2px solid");
+			 $( phoneNo ).attr("data-check", "invalidDataNoneClick");
 		 }
 	 });
 });
 
 $( "#phoneNo" ).focusout(function() {
 	var phoneNo = $( "#phoneNo" );
-	console.log("-----------------");
-	console.log($( phoneNo ).css("border-color") == "rgb(0, 128, 0)");
 	
 	if(  $( phoneNo ).css("border-color") == "rgb(0, 128, 0)" ){ //초록색 
 		$( ".phoneNoValid" ).css("display","none");
 	}
 });
 
+/*--------------------핸드폰 번호 인증번호 검사-----------------*/
 
-
-
+$( "#confirmVerCode" ).focus(function() {
+	var confirmVerCode = $( "#confirmVerCode" );
+	
+	$( confirmVerCode ).keyup(function(){
+		
+		if($( confirmVerCode ).val() == randomNo){
+			updateUserMap.phoneNo = $("#phoneNo").val();  //핸드폰 인증까지 만료 하면 변경될 핸드폰 값 넣기
+	
+			$( confirmVerCode ).css("border","green 2px solid");
+			$( ".confimNoValid" ).css("display", "none");
+			$( confirmVerCode ).attr("data-check", "validData");
+			 
+		 } else {
+			 $( confirmVerCode ).css("border","red 2px solid");
+			 $( ".confimNoValid" ).html("인증번호가 맞지 않습니다").css("display", "block").css("color", "red");
+			 $( confirmVerCode ).attr("data-check", "invalidData");
+		 }
+	});
+});
+	
+$( "#confirmVerCode" ).focus(function() {
+	var confirmBtn = $( "#confirmBtn" );
+	
+	if(  $( confirmBtn ).css("border-color") == "rgb(0, 128, 0)" ){ //초록색 
+		$( ".confimNoValid" ).css("display","none");
+	}
+});
 
 
 
