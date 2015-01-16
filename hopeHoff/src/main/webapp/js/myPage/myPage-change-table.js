705d60fce069b70533a6c86781bc544761c3da5b
Narae <Narae@Narae-PC>
2015-01-16 16:02:48 +0900

    20150116 v1.0.0 -narae
    1) 마이페이지에서 입력하는 것 유효성검사 가능
    (아직 핸드폰번호 입력했을 시, 인증번호 보내는 것 불가능 )

---
+++ hopeHoff/src/main/webapp/js/myPage/myPage-change-table.js
@@ -0,0 +1,103 @@
+var pwdStatus = false;  //수정 버튼 눌렀을 때의 체크를 위함
+var randomNo = Math.ceil( Math.random() * 1000000 );
+
+$( "#sendBtn" ).click(function() {
+	console.log("전송버튼 눌렀다");
+});
+
+
+/* --------------password 유 효 성--------------- */
+$( '#newPwd' ).focus( function() {
+	var newPwd = $( "#newPwd" );
+		
+	$('.pwdValid').css("display","");
+	$( newPwd ).keyup(function(){
+		if(($(newPwd ).val().match(/([a-zA-Z0-9].*[!,@,#,$,%,^,&,*,?,_,~])|([!,@,#,$,%,^,&,*,?,_,~].*[a-zA-Z0-9])/) != null)
+					 && (($( newPwd).val().length >= 6)&&($( newPwd ).val().length <= 16) )){
+				
+			$( ".pwdValid" ).html("ok").css("color","green");
+			$( newPwd ).css("border","green 2px solid");
+	
+		} else {
+			$( ".pwdValid" ).html("문자, 숫자, 특수문자(!,@,#,$,%,^,&,*,?,_,~의 조합으로 6~16자리").css("color","red");
+			$( newPwd ).css("border","red 2px solid");
+				
+		}
+	});
+});
+
+$( '#newPwd' ).focusout( function() {
+	var newPwd = $( "#newPwd" );
+	
+	if(  $( newPwd ).css("border-color") == "rgb(0, 128, 0)"){ //초록색 
+		$( ".pwdValid" ).css("display","none");
+	}
+});
+
+$( "#newPwdConfirm" ).focus( function() {
+	var newPwdConfirm = $( "#newPwdConfirm" );
+	
+	$( newPwdConfirm ).keyup(function() {
+		if( newPwdConfirm.val() == $( "#newPwd" ).val() ){
+			pwdStatus = "success"; 
+
+			$( ".pwdConfirmValid" ).html("암호가 일치합니다").css("color", "green");
+			$( newPwdConfirm ).css("border","green 2px solid");
+		}else {
+			$( ".pwdConfirmValid" ).html("암호가 일치하지 않습니다").css("color", "red");
+			$( newPwdConfirm ).css("border","red 2px solid");
+		}
+	})
+});
+
+$( "#newPwdConfirm" ).focusout( function() {
+	var newPwdConfirm = $( "#newPwdConfirm" );
+	
+	if(  $( newPwdConfirm ).css("border-color") == "rgb(0, 128, 0)"){ //초록색 
+		$( ".pwdConfirmValid" ).css("display","none");
+	}
+});
+
+
+/*--------------------핸드폰 번호 유효성 검사-----------------*/
+$( "#phoneNo" ).focus(function() {
+	var phoneNo = $( "#phoneNo" );
+	
+	 //$('#phoneNumValid').css("display","");
+	 
+	 $( phoneNo ).keyup(function(){
+		 if( ( phoneNo ).val().match(/[01](0|1|6|7|8|9)[-](\d{4}|\d{3})[-]\d{4}$/) != null){
+			 $( ".phoneNoValid" ).html("ok").css("color","green");
+			 $( phoneNo ).css("border","green 2px solid");
+			// $( "#sendBtn" ).attr("disabled", "false");
+			 
+		 } else {
+			 $( ".phoneNoValid" ).html("유효한 핸드폰 번호가 아닙니다.  예)010-1234-4567").css("color","red");
+			 $( phoneNo ).css("border","red 2px solid");
+			 //$( "#sendBtn" ).attr("disabled", "true");
+		 }
+	 });
+});
+
+$( "#phoneNo" ).focusout(function() {
+	var phoneNo = $( "#phoneNo" );
+	console.log("-----------------");
+	console.log($( phoneNo ).css("border-color") == "rgb(0, 128, 0)");
+	
+	if(  $( phoneNo ).css("border-color") == "rgb(0, 128, 0)" ){ //초록색 
+		$( ".phoneNoValid" ).css("display","none");
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
--- hopeHoff/src/main/webapp/js/myPage/myPage.js
+++ hopeHoff/src/main/webapp/js/myPage/myPage.js
@@ -1,32 +1,19 @@
 $(function(){
-	//******************** btnClose 클릭시에 ************************//
-	$("#btnMyPageClose").click(function(){
-		$("#back").css("display", "none");
-		$("#myPage").css("display", "none");
-	});
 
 });
 
-/*$.getJSON('/hopeHoff/json/auth/loginUser.do', function(data){
-   //console.log("what the f***"+data.status);
-	if (data.status == "fail") {
-		console.log("fail..");
-	} else { //success
-		$(".userId").attr("placeholder"," "+data.loginUser.uId);
-		$(".userName").attr("placeholder"," "+data.loginUser.uName);
-	}
-});*/
-
+$("#btnMyPageClose").click(function(){
+	$("#back").css("display", "none");
+	$("#myPage").css("display", "none");
+});
 
 $("#btnPwdConfirm").click(function(){
 	$.getJSON('/hopeHoff/json/auth/chkUser.do', 
 			{'userId' : $("#chkUserId").val(),
 		'userPwd': $("#chkUserPwd").val() },
 			function(data){
-			console.log(data);
-			
 			if(data.status == 'success'){
-				require(['text!templates/myPage-table.html'], function(html){
+				require(['text!templates/myPage-change-table.html'], function(html){
 					var template = Handlebars.compile(html);
 					$('#myPageContainer').html( template(data) );
 				});
@@ -35,4 +22,5 @@ $("#btnPwdConfirm").click(function(){
 			}
 	});
 })
+
 
